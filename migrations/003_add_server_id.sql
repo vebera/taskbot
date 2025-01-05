8fca6517f0b0fdc858a7841b8de2967068ab88e2
vasyl <vasyl@corebanq.com>
2025-01-05 22:49:01 +0100

    hotfix

--- migrations/003_add_server_id.sql
+++ migrations/003_add_server_id.sql
@@ -1,9 +1,3 @@
--- Add server_id column to tasks table
-ALTER TABLE tasks ADD COLUMN server_id VARCHAR(64);
-
--- Add server_id column to check_ins table
-ALTER TABLE check_ins ADD COLUMN server_id VARCHAR(64);
-
 -- Set default server_id for existing records (using the first server from server_settings)
 WITH first_server AS (
     SELECT server_id FROM server_settings LIMIT 1
@@ -17,10 +11,32 @@ WITH first_server AS (
 UPDATE check_ins SET server_id = (SELECT server_id FROM first_server)
 WHERE server_id IS NULL;
 
--- Make server_id NOT NULL after setting defaults
-ALTER TABLE tasks ALTER COLUMN server_id SET NOT NULL;
-ALTER TABLE check_ins ALTER COLUMN server_id SET NOT NULL;
+-- Make server_id NOT NULL if not already
+DO $$ 
+BEGIN
+    BEGIN
+        ALTER TABLE tasks ALTER COLUMN server_id SET NOT NULL;
+    EXCEPTION
+        WHEN others THEN NULL;
+    END;
+    BEGIN
+        ALTER TABLE check_ins ALTER COLUMN server_id SET NOT NULL;
+    EXCEPTION
+        WHEN others THEN NULL;
+    END;
+END $$;
 
--- Add indexes for server_id columns
-CREATE INDEX IF NOT EXISTS idx_tasks_server_id ON tasks(server_id);
-CREATE INDEX IF NOT EXISTS idx_check_ins_server_id ON check_ins(server_id); 
+-- Add indexes if they don't exist
+DO $$ 
+BEGIN
+    BEGIN
+        CREATE INDEX idx_tasks_server_id ON tasks(server_id);
+    EXCEPTION
+        WHEN duplicate_table THEN NULL;
+    END;
+    BEGIN
+        CREATE INDEX idx_check_ins_server_id ON check_ins(server_id);
+    EXCEPTION
+        WHEN duplicate_table THEN NULL;
+    END;
+END $$; 
